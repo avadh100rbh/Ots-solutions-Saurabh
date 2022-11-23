a862c87585addc7eecc0a7ec60609ef6709378d7
Saurabh Singh <saurabhsingh@Saurabhs-Mac-mini.local>
2022-11-23 12:19:36 +0530

    homework

--- BMICalc/app.js
+++ BMICalc/app.js
@@ -1,33 +1,44 @@
-let previousBMIs = [];
+// let previousBMIs = [];
 
-let randomNumber = Math.ceil(Math.random() * 100 + 1);
+// let randomNumber = Math.ceil(Math.random() * 100 + 1);
 
 
 
-// Main Function
+// // Main Function
 
-function checkGuess() {
+// function checkGuess() {
 
-  let weight = parseInt(document.getElementById("guessField").value);
+//   let weight = parseInt(document.getElementById("guessField").value);
 
-  let height = parseFloat(document.getElementById("heightField").value);
+//   let height = parseFloat(document.getElementById("heightField").value);
 
-  document.getElementById("guessField").value=''
+//   document.getElementById("guessField").value=''
 
-  document.getElementById("heightField").value=''
+//   document.getElementById("heightField").value=''
 
-  let BMI= weight/Math.pow(height,2)
+//   let BMI= weight/Math.pow(height,2)
 
-  previousBMIs.push(BMI.toFixed(2))
+//   previousBMIs.push(BMI.toFixed(2))
 
-  const guessesRemaining = document.querySelector(".lastResult").innerHTML=BMI.toFixed(2);
+//   const guessesRemaining = document.querySelector(".lastResult").innerHTML=BMI.toFixed(2);
 
-  const guessSlot = document.querySelector(".guesses").innerHTML=previousBMIs;
+//   const guessSlot = document.querySelector(".guesses").innerHTML=previousBMIs;
 
-if (previousBMIs.length==10) {
+// if (previousBMIs.length==10) {
 
-    previousBMIs.splice(0,1)
+//     previousBMIs.splice(0,1)
 
-}
+// }
 
-}
+// }
+
+
+
+// const api = fetch('https://restcountries.com/v2/name/')
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+
+ const Api = fetch('https://restcountries.com/v2/name/india')
+ .then((data)=>data.json())
+ .then((response)=>console.log(response[1].borders)
+ )
