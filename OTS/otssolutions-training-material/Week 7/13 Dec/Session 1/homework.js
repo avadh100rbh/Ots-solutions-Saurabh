3517b11983291b5c1db2bbdf0b2075152d4686de
avadh100rbh <saurabhsinghrathore1997@gmail.com>
2022-12-21 15:20:52 +0530

    update training meterial

---
+++ OTS/otssolutions-training-material/Week 7/13 Dec/Session 1/homework.js
@@ -0,0 +1,28 @@
+/*
+
+Today's Homework -
+
+1. Add Dropdown for Payment Mode in Currency Selector Application
+2. Read about Controlled v/s Uncontrolled Components
+
+   Link - https://goshacmd.com/controlled-vs-uncontrolled-inputs-react/
+
+3. Read about Render Props
+
+*/
+
+/*
+   
+currentPage, setCurrentPage -> 1
+
+<button onClick={() => setCurrentPage(1)}>1</button>
+<button>2</button>
+<button>3</button>
+<button>4</button>
+
+1 -> 0 to 4
+2 -> 5 to 9
+3 -> 10 to 14
+4 -> 15 to 19
+
+*/
